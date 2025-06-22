4ef128c875e17992df84fc31f85d3ccee678a056
Baaron237 <baaronjc@gmail.com>
2025-06-22 18:08:32 +0100

    modification sur le store context

--- frontend/src/context/StoreContext.tsx
+++ frontend/src/context/StoreContext.tsx
@@ -1,61 +1,63 @@
-/* eslint-disable react-refresh/only-export-components */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext, useEffect, useState} from "react"
+import { createContext, useEffect, useState } from "react";
 
+export const StoreContext = createContext<any>(null);
 
-export const StoreContext = createContext<any>(null)
+const StoreContextProvider = ({ children }: { children: any }) => {
+  const [token, setToken] = useState<string | null>(() =>
+    localStorage.getItem("AUTH_TOKEN")
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<any | null>(() => {
+    const storedUser = localStorage.getItem("USER_DATA");
+    if (storedUser) {
+      try {
+        return JSON.parse(storedUser);
+      } catch (error) {
+        console.error("Erreur de parsing USER_DATA :", error, storedUser);
+        return null; // Valeur par défaut en cas d'erreur
+      }
+    }
+    return null;
+  });
 
-const StoreContextProvider = ({ children } : { children: any}) => {
+  const logout = () => {
+    localStorage.removeItem("AUTH_TOKEN");
+    localStorage.removeItem("USER_DATA");
+    setUser(null);
+    setToken(null);
+  };
 
-    const [token, setToken] = useState<string | null>(() => localStorage.getItem('AUTH_TOKEN'));
-    const [isLoading, setIsLoading] = useState(false);
-    const [user, setUser] = useState<any | null>(() => {
-        const storedUser = localStorage.getItem('USER_DATA');
-        return storedUser ? JSON.parse(storedUser) : null;
-    });
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("AUTH_TOKEN", token);
+    } else {
+      localStorage.removeItem("AUTH_TOKEN");
+    }
+  }, [token]);
 
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("USER_DATA", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("USER_DATA");
+    }
+  }, [user]);
 
-    const logout = () => {
-        localStorage.removeItem("AUTH_TOKEN");
-        localStorage.removeItem("USER_DATA");   
-        setUser(null);
-        setToken(null);
-    };
+  const contextValue = {
+    token,
+    user,
+    setUser,
+    setToken,
+    isLoading,
+    setIsLoading,
+    logout,
+  };
 
-    useEffect(() => {
-        if (token) {
-            localStorage.setItem('AUTH_TOKEN', token);
-        } else {
-            localStorage.removeItem('AUTH_TOKEN');
-        }
-    }, [token]);
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
-    useEffect(() => {
-        if (user) {
-            localStorage.setItem('USER_DATA', JSON.stringify(user));
-        } else {
-            localStorage.removeItem('USER_DATA');
-        }
-        }, [user]
-    );
-
-
-    const contextValue = {
-        token,
-        user,
-        setUser,
-        setToken,
-        isLoading,
-        setIsLoading,
-        logout,
-    };
-
-
-    return (
-        <StoreContext.Provider value={contextValue}>
-            {children}
-        </StoreContext.Provider>
-    )
-}
-
-export default StoreContextProvider
+export default StoreContextProvider;
